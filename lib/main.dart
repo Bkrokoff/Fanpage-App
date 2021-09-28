46cffc2ef312b1cfe99b3bb19cf08a59bfdf57dd
Brett Krokoff <Bkrokoff1@student.gsu.edu>
2021-09-28 13:16:05 -0400

    Firebase Installation

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:aotfanpage/myhomepage.dart';
 import 'package:flutter/material.dart';
 
 
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  MyApp({Key? key}) : super(key: key);
+
+  final Future<FirebaseApp> _intialization = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,7 +30,18 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: FutureBuilder(
+        future: _intialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            print("Error");
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            return MyHomePage(title: 'Flutter Demo Home Page');
+          }
+          return CircularProgressIndicator();
+        },
+      )
     );
   }
 }
